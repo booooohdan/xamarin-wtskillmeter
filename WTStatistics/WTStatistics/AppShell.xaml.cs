b3ab7495422eb6e574659e5960d8f13ea2865796
Bogdan <bo7145907@gmail.com>
2020-09-30 21:10:03 +0300

    Добавьте файлы проекта.

---
+++ WTStatistics/WTStatistics/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using WTStatistics.ViewModels;
+using WTStatistics.Views;
+using Xamarin.Forms;
+
+namespace WTStatistics
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
