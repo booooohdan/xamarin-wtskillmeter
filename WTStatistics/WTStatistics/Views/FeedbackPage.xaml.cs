fd9d566d002c4da006538e5d39b08002ac6c3830
Bohdan Ilkiv <bohdan.ilkiv@globallogic.com>
2020-11-11 16:10:10 +0200

    ios localization and splash added

--- WTStatistics/WTStatistics/Views/FeedbackPage.xaml.cs
+++ WTStatistics/WTStatistics/Views/FeedbackPage.xaml.cs
@@ -28,24 +28,7 @@ namespace WTStatistics.Views
         // RatingBar value change handler
         private void SfRating_ValueChanged(object sender, Syncfusion.SfRating.XForms.ValueEventArgs e)
         {
-            switch (Device.RuntimePlatform)
-            {
-                case Device.Android:
-                    CrossStoreReview.Current.RequestReview();
-                    break;
-                case Device.iOS:
-                    //Launcher.OpenAsync(new Uri("https://play.google.com/store/apps/details?id=com.wave.skillmeter"));
-                    /*ar url = $"itms-apps://itunes.apple.com/app/id{appId}?action=write-review";
-                    try
-                    {
-                        UIApplication.SharedApplication.OpenUrl(new NSUrl(url));
-                    }
-                    catch (Exception ex)
-                    {
-                        Debug.WriteLine("Unable to launch app store: " + ex.Message);
-                    }*/
-                    break;
-            }
+            CrossStoreReview.Current.RequestReview();
         }
     }
 }
