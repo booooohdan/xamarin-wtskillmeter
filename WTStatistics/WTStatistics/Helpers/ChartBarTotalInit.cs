17bc748a89a426fa083887cc893493470d7a93aa
Bogdan <bo7145907@gmail.com>
2020-11-13 16:59:06 +0200

    Researchable vehicle chart working well

--- WTStatistics/WTStatistics/Helpers/ChartBarTotalInit.cs
+++ WTStatistics/WTStatistics/Helpers/ChartBarTotalInit.cs
@@ -12,13 +12,13 @@ namespace WTStatistics.Helpers
         {
             var collection = new ObservableCollection<BarChartDataModel>
             {
-                new BarChartDataModel("", 300),
-                new BarChartDataModel("", 300),
-                new BarChartDataModel("", 300),
-                new BarChartDataModel("", 300),
-                new BarChartDataModel("", 300),
-                new BarChartDataModel("", 300),
-                new BarChartDataModel("", 300)
+                new BarChartDataModel("", 125),
+                new BarChartDataModel("", 141),
+                new BarChartDataModel("", 192),
+                new BarChartDataModel("", 242),
+                new BarChartDataModel("", 329),
+                new BarChartDataModel("", 297),
+                new BarChartDataModel("", 282)
             };
             return collection;
         }
